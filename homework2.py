2edcaa0ccd33863be8574b631c48bc7e6139c7d5
hohlov-d <hohlov_d.o@mail.ru>
2024-05-28 22:52:50 +0500

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -2,4 +2,4 @@ number_of_completed_tasks = 12
 number_of_hours_spent = 1.5
 name = 'Python'
 time_for_one_task = number_of_hours_spent / number_of_completed_tasks
-print('Курс:',name,',всего задач:',number_of_completed_tasks,',затрачено часов:',number_of_hours_spent,',среднее время выполнения',time_for_one_task)
+print('Курс: ',name,', всего задач: ',number_of_completed_tasks,', затрачено часов: ',number_of_hours_spent,', среднее время выполнения ',time_for_one_task, sep='')
