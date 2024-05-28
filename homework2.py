c99ab80c944a39d5f538f1e2858fd2356651be8a
hohlov-d <hohlov_d.o@mail.ru>
2024-05-28 22:22:59 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_completed_tfsks = 12
+number_of_hours_spent = 1.5
+name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_completed_tfsks
+print('Курс:',name,',всего задач:',number_of_completed_tfsks,',затрачено часов:',number_of_hours_spent,',среднее время выполнения',time_for_one_task)
